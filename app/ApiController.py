e21fc527189649cfc00ddd1a67900ced93bb4e13
Kurt <akurt6805@protonmail.com>
2024-06-03 23:34:08 -0700

    D2RS-3 Api call wrapper function

--- app/ApiController.py
+++ app/ApiController.py
@@ -1,5 +1,5 @@
 from typing import Dict
-import requests
+import requests, json
 from app.data.classhash import CLASS_HASH
 
 API_ROOT_PATH = "https://www.bungie.net/Platform"
@@ -9,19 +9,55 @@ class ApiController:
 
     __HEADERS: Dict[str, str]
 
-
+    ###########################################################################
+    #
+    # Base API
+    #
+    ###########################################################################    
     def __init__(self, api_key: str):
         self.__HEADERS = {"X-API-Key": api_key}
         pass
 
 
-    def getProfile(self, membershipType, destinyMembershipId, components=[100]):
+    def __call_api(self, method: str, apiString: str, params = None, timeout = None):
+        import time
+        for i in range(0, 3):
+            call = requests.request(method=method, url=apiString, headers=self.__HEADERS, params=params, timeout=timeout)
+
+            # break if it has no error
+            if call.status_code // 100 == 2:
+                return (call.json())['Response']
+            
+            errorJson = json.loads(call.text)
+            if 'ErrorCode' in errorJson:
+                errorCode = errorJson['ErrorCode']
+                if errorCode == 1665: # privacy settings enabled for user
+                    return errorJson
+            
+            # wait and try again
+            DELAY = 5
+            print(f"Attemp {i + 1} failed. Error: {call.status_code}. Waiting {DELAY} seconds and trying again.")
+            time.sleep(DELAY)
+        print(f"API calls failed. Exiting...")
+        exit(2)
+
+
+    ###########################################################################
+    #
+    # Public functions
+    #
+    ###########################################################################
+    def getProfile(self, membershipType, destinyMembershipId):
         params = {}
-        if components is not None: params["components"] = components
+        params["components"] = [100]
 
-        api_call = requests.get(f'{API_ROOT_PATH}/Destiny2/{membershipType}/Profile/{destinyMembershipId}', headers=self.__HEADERS, params=params)
+        call = self.__call_api(
+            method='GET', 
+            apiString=f'{API_ROOT_PATH}/Destiny2/{membershipType}/Profile/{destinyMembershipId}',
+            params=params
+        )
 
-        return (api_call.json())['Response']
+        return call
 
 
     def getAccountStats(self, membershipType, destinyMembershipId):
